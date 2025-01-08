70864c51fd6d6ff3c347ffc69c62ff34d2d7658f
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2025-01-08 11:18:14 +0700

    feat: Constructor Parameter

--- src/oop/Person.java
+++ src/oop/Person.java
@@ -3,4 +3,9 @@ public class Person {
     int age;
     String name;
     public Person() {}
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 }
